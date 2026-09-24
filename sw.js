4ed57f5c03ade0eb3d73d7492cd322029fef0991
jadyakub <tunejat88@gmail.com>
2026-09-24 19:49:33 +0800

    Bump cache for OTDR fault locator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'automation-fiber-trace-v2';
+const CACHE = 'automation-fiber-trace-v3';
 const CORE = [
   '/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/assets/app-icon.svg',
   '/data/f14-kgu-c029m-1.geojson', '/data/f14-kgu-c029m-2.geojson',
